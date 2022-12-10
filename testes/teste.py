627a5cae80680453258c4be536462c3ffefc71f1
samur2d2 <samuribeiro@ucl.br>
2022-12-10 00:26:25 -0300

    leitura do arquivo e separação em 3 listas
    lê o arquivo por completo e separa n valores, determinados por "defineRate", em 3 listas distintas.

--- testes/teste.py
+++ testes/teste.py
@@ -1,9 +1,20 @@
-dados = "./ArquivosHistorico/192.168.0.10/input_2022_12_10.txt"
+import IHMfiles.returnRequests as Request
+listaItens, listaHora, listaInput, listaSetpoint = [], [], [], []
 definedRange = 4
-listaItens = []
+
+#------------- LÊ O IP DO EQUIPAMENTO SELECIONADO NA IHM --------------------
+#------------- VERIFICA SE O DIRETORIO DESTE IP EXISTE, SE NÃO ELE É CRIADO --------------------
+with open("./IHMfiles/fileIP.txt", 'r') as fonte:
+    IpOnFile = fonte.read()
+    folder = Request.VerifyDir(IpOnFile)
+
+#------------- CRIA AS VARIÁVEIS PARA ABRIR OS ARQUIVOS COM OS DADOS ATUAIS --------------------
+day, hour = Request.getTime()
+lerDados = folder[0:]+ "/input_" +day+ ".txt"
+dadosLast = folder[0:]+ "/LASTinput_" +day+ ".txt"
 
 #------------- LÊ TODOS OS DADOS DE UM TXT E SUBSTITUI A QUEBRA DE LINHA POR ESPAÇO --------------------
-with open(dados, 'r') as fonte:
+with open(lerDados, 'r') as fonte:
     dadosFile = fonte.read()
 dadosFile = dadosFile.replace('\n', " ")
 
@@ -14,4 +25,11 @@ listaItens.reverse()
 
 #------------- SEPARA OS ITENS ATÉ O LIMITE PRE DEFINIDO (definedRange) --------------------
 listaItens = listaItens[:definedRange]
-print(listaItens)
+
+#------------- SEPARA TODOS OS ITEMS EM 3 LISTAS DISTINTAS PARA CADA EIXO DO GRAFICO --------------------
+for item in listaItens:
+    hora, input, setpoint = item.split(",")
+    listaHora.append(hora)
+    listaInput.append(input)
+    listaSetpoint.append(setpoint)
+print(listaHora)
