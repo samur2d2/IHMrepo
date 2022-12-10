c64e2a9a333850aef2d25e8072732514094ac4c4
samur2d2 <samuribeiro@ucl.br>
2022-12-10 02:36:35 -0300

    função para centralizar a janela

--- IHMfiles/returnRequests.py
+++ IHMfiles/returnRequests.py
@@ -12,3 +12,21 @@ def VerifyDir(IPSender):
     if (os.path.exists("./ArquivosHistorico/"+IPSender)) != True:
         os.makedirs("./ArquivosHistorico/"+IPSender)
     return "./ArquivosHistorico/"+IPSender
+
+def Centralizar(win):
+    win.update_idletasks()
+
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y-25))
+
+    win.deiconify()
